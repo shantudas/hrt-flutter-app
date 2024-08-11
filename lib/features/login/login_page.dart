7a25284afad3c406e4c6bbe84661102cbdf5c1fd
shantudas <shantuchandradas@yahoo.com>
2024-08-11 13:41:52 +0600

    Get localization implemented

--- lib/features/login/login_page.dart
+++ lib/features/login/login_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:hrt_flutter_app/controller/localization_controller.dart';
+import 'package:hrt_flutter_app/features/home/home_page.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,26 +16,37 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-  LocalizationController languageController = Get.find();
+  LocalizationController localizationController = Get.find();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppLocalizations.of(context)!.appTitle),
+        title: Text(AppLocalizations.of(context)!.loginPageAppBarTitle),
       ),
       body: Center(
-        child: DropdownButton<Locale>(
-          icon: const Icon(Icons.settings, color: Colors.black),
-          value: languageController.locale,
-          items: languageController.supportedLocales.map((locale) {
-            return DropdownMenuItem(
-              value: locale,
-              child: Text(locale.languageCode), // Use language code for display
-            );
-          }).toList(),
-          onChanged: languageController.setLocale,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            DropdownButton<Locale>(
+              icon: const Icon(Icons.settings, color: Colors.black),
+              value: localizationController.locale,
+              items: localizationController.supportedLocales.map((locale) {
+                return DropdownMenuItem(
+                  value: locale,
+                  child: Text(
+                      locale.languageCode), // Use language code for display
+                );
+              }).toList(),
+              onChanged: localizationController.setLocale,
+            ),
+            TextButton(
+              onPressed: () {
+                Get.to(HomePage());
+              },
+              child: Text(AppLocalizations.of(context)!.loginPageButton),
+            ),
+          ],
         ),
       ),
     );
